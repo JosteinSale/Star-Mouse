f0e9c73814ee38f9a0f79370fe31182b8d93da32
josteinSale <josteinsale@hotmail.com>
2024-01-13 15:41:18 +0100

    level1_bg spaaace

--- src/gamestates/Flying.java
+++ src/gamestates/Flying.java
@@ -84,7 +84,7 @@ public class Flying extends State implements Statemethods {
         loadPickupItems(level);
         loadCutscenes(level);  
         player.setKilledEnemies(0);
-        //startAt(-23500);
+        startAt(-23500);
     }
 
     private void initClasses(OptionsMenu optionsMenu) {
