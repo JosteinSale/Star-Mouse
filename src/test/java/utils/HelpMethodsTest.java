3e46f512bd8553ce0e8ec90f4e90a3f8465fcd44
Jostein Sæle <josteinsale@hotmail.com>
2026-03-20 19:38:32 +0100

    Added Junit dependency and the project's first test :3

---
+++ src/test/java/utils/HelpMethodsTest.java
@@ -0,0 +1,14 @@
+package utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class HelpMethodsTest {
+
+   @Test
+   public void testIsSolid() {
+      System.out.println("printing debug info to the debug console");
+      assertEquals(1, 1);
+   }
+}
